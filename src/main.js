3adc3be30dbeb6f624f8cd733eba3e63a32fd015
sudo-shihab <contact.shihab.me@gmail.com>
2020-05-20 04:38:31 +0530

    minor fixes

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import store from './store';
 // ────────────────────────────────────────────────────────────────────────────────
 
 Vue.config.productionTip = false;
+console.log('test');
 
 new Vue({
   router,
