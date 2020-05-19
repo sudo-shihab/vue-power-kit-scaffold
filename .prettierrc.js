b60ddc2879156d2e806931535dbd2c5274589460
sudo-shihab <contact.shihab.me@gmail.com>
2020-05-19 21:59:39 +0530

    added eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 140,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  proseWrap: 'always'
+};
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -30,3 +30,13 @@
   }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'hello'
+    };
+  }
+};
+</script>
