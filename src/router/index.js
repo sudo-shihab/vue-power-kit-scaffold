0047b1455ee4f36e751c0577f7fa2c12e1fbdfae
sudo-shihab <contact.shihab.me@gmail.com>
2020-05-21 06:20:01 +0530

    comments sections sample added

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,10 @@ const routes = [
   }
 ];
 
+/**
+ * @description - A first class function to encapsulate routes config
+ * @param  {Array} {routes}
+ */
 const router = new VueRouter({
   routes
 });
