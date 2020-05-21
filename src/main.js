6ad735d35e0b653d9dedcd89139204921205fe1d
sudo-shihab <contact.shihab.me@gmail.com>
2020-05-21 06:36:22 +0530

    comments sections edited

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 /*
  
-                                                                               
-  #    # #    # ######    #####   ####  #    # ###### #####     #    # # ##### 
-  #    # #    # #         #    # #    # #    # #      #    #    #   #  #   #   
-  #    # #    # #####     #    # #    # #    # #####  #    #    ####   #   #   
-  #    # #    # #         #####  #    # # ## # #      #####     #  #   #   #   
-   #  #  #    # #         #      #    # ##  ## #      #   #     #   #  #   #   
-    ##    ####  ######    #       ####  #    # ###### #    #    #    # #   #   
-                                                                               
+                                                                                                                  
+ .s    s.  .s    s.  .s5SSSs.      .s5SSSs.  .s5SSSs.  .s s.  s.  .s5SSSs.  .s5SSSs.      .s    s.  s.  .s5SSSSs. 
+       SS.       SS.       SS.           SS.       SS.    SS. SS.       SS.       SS.           SS. SS.    SSS    
+ sS    S%S sS    S%S sS    `:;     sS    S%S sS    S%S sS S%S S%S sS    `:; sS    S%S     sS    S%S S%S    S%S    
+ SS    S%S SS    S%S SSSs.         SS .sS::' SS    S%S SS S%S S%S SSSs.     SS .sS;:'     SSSSs.S:' S%S    S%S    
+  SS   S%S SS    S%S SS            SS        SS    S%S SS S%S S%S SS        SS    ;,      SS  "SS.  S%S    S%S    
+  SS   `:; SS    `:; SS            SS        SS    `:; SS `:; `:; SS        SS    `:;     SS    `:; `:;    `:;    
+   SS  ;,. SS    ;,. SS    ;,.     SS        SS    ;,. SS ;,. ;,. SS    ;,. SS    ;,.     SS    ;,. ;,.    ;,.    
+    `:;;:' `:;;;;;:' `:;;;;;:'     `:        `:;;;;;:' `:;;:'`::' `:;;;;;:' `:    ;:'     :;    ;:' ;:'    ;:'    
+                                                                                                                  
  
 */
+
+//
+// ────────────────────────────────────────────────────────────────────────────────────────── I ──────────
+//   :::::: M A I N   J S   F I L E   -   E N T R Y   F I L E : :  :   :    :     :        :          :
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
+//
 
 /**
  * @license
@@ -20,18 +28,13 @@
  */
 
 //
-// ──────────────────────────────────────────────────────────── I ──────────
-//   :::::: E N T R Y   F I L E   M O D U L E : :  :   :    :     :
-// ──────────────────────────────────────────────────────────────────────
-//
-
-//
 // ─── IMPORTS ────────────────────────────────────────────────────────────────────
 //
-// NPM PACKAGES
+
+// npm packages
 import Vue from 'vue';
 
-// APP PACAKGES
+// app imports
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -41,10 +44,10 @@ import store from './store';
 // ─── SCRIPT LOGIC ───────────────────────────────────────────────────────────────
 //
 
-// CONFIG FOR PROD TIPS
+// config for prod wrt to vue cli
 Vue.config.productionTip = false;
 
-// INTIALIZE VUE INSTANCE
+// intialize vue instance
 new Vue({
   router,
   store,
